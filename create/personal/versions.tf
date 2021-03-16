fcea5221addd53bc79dc265fbe058340788d7e81
Siddhartha Basu <biosidd@gmail.com>
2021-03-16 12:04:30 -0500

    feat: add local_file provider

--- create/personal/versions.tf
+++ create/personal/versions.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/google-beta"
       version = "3.58.0"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
   }
   backend "gcs" {}
   required_version = "~> 0.14"
