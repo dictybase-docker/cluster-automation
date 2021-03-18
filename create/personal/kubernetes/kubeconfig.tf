9f7716a642beef203feb2564f452b0f5064343d7
Siddhartha Basu <biosidd@gmail.com>
2021-03-17 19:38:58 -0500

    refactor: reorganize files for gke module

--- create/personal/kubernetes/kubeconfig.tf
+++ create/personal/kubernetes/kubeconfig.tf
@@ -11,7 +11,8 @@ resource "local_file" "kubeconfig" {
       cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate,
       endpoint               = google_container_cluster.primary.endpoint,
       context                = google_container_cluster.primary.name,
-      token                  = data.google_client_config.client.access_token
+      client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate,
+      client_key             = google_container_cluster.primary.master_auth.0.client_key,
   })
   filename = var.kubeconfig_output_file
 }
