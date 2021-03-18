93c2fda44bacf99183ff6d094c9259eaf2f4e2f9
Siddhartha Basu <biosidd@gmail.com>
2021-03-17 19:46:47 -0500

    feat: add kubernetes provider

--- create/personal/versions.tf
+++ create/personal/versions.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/local"
       version = "2.1.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.0.2"
+    }
   }
   backend "gcs" {}
   required_version = "~> 0.14"
