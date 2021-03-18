ceeb24f404b20ffa0b9329489fbe881865244c6e
Siddhartha Basu <biosidd@gmail.com>
2021-03-17 19:44:14 -0500

    refactor: edit output vars so that it could be passed to kubernetes module

--- create/personal/gke/outputs.tf
+++ create/personal/gke/outputs.tf
@@ -1,19 +1,17 @@
-output "region" {
-  value       = var.region
-  description = "GCloud Region"
+output "cluster_id" {
+  value       = google_container_cluster.primary.id
 }
 
-output "project_id" {
-  value       = var.project_id
-  description = "GCloud Project ID"
-}
-
-output "kubernetes_cluster_name" {
+output "cluster_name" {
   value       = google_container_cluster.primary.name
   description = "GKE Cluster Name"
 }
 
-output "kubernetes_cluster_host" {
+output "cluster_endpoint" {
   value       = google_container_cluster.primary.endpoint
   description = "GKE Cluster Host"
+}
+
+output "cluster_ca_cert" {
+  value       = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
 }
