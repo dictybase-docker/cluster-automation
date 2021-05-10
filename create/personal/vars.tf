e118cf9c73fcf3a5a4ee8b9ca622684d3888cb43
Siddhartha Basu <biosidd@gmail.com>
2021-05-10 17:59:09 -0500

    fix: add missing kubeconfig template variable

--- create/personal/vars.tf
+++ create/personal/vars.tf
@@ -69,3 +69,8 @@ variable "kubeconfig_output_file" {
   default     = "kubeconfig.yaml"
   description = "name of kubeconfig file to access cluster"
 }
+
+variable "kubeconfig_template_file" {
+  default     = "kubernetes/template/kubeconfig-template.yaml.tpl"
+  description = "template for generating kubernetes config file"
+}
