0b16f16374b5265591838e6c1e38f8cf9434d37e
Siddhartha Basu <biosidd@gmail.com>
2021-05-10 14:39:54 -0500

    fix: fix module names

--- create/personal/main.tf
+++ create/personal/main.tf
@@ -1,9 +1,9 @@
 module "gke-cluster" {
-  source       = "./gke-cluster"
+  source       = "./gke"
 }
 
 module "kubernetes-config" {
-  source           = "./kubernetes-config"
+  source           = "./kubernetes"
   cluster_name     = module.gke-cluster.cluster_name
   cluster_id       = module.gke-cluster.cluster_id # creates dependency on cluster creation
   cluster_endpoint = module.gke-cluster.cluster_endpoint
