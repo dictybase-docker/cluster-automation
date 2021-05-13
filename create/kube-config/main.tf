ea0f67f36dcf80498c102c299ec1c907f35d0526
wildlifehexagon <hartline.eric@gmail.com>
2021-05-13 17:08:14 -0500

    chore: run terraform init

--- create/kube-config/main.tf
+++
@@ -1,9 +0,0 @@
-module "kubernetes-config" {
-  source                   = "./kubernetes"
-  cluster_name             = module.gke-cluster.cluster_name
-  cluster_id               = module.gke-cluster.cluster_id # creates dependency on cluster creation
-  cluster_endpoint         = module.gke-cluster.cluster_endpoint
-  cluster_ca_cert          = module.gke-cluster.cluster_ca_cert
-  kubeconfig_output_file   = var.kubeconfig_output_file
-  kubeconfig_template_file = var.kubeconfig_template_file
-}
