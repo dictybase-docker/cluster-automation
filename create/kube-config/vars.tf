34e75d762456ede8c7ecc491ae30e1ca2952a175
wildlifehexagon <hartline.eric@gmail.com>
2021-05-13 13:55:11 -0500

    refactor: move vars into correct file

--- create/kube-config/vars.tf
+++ create/kube-config/vars.tf
@@ -10,3 +10,12 @@ variable "kubeconfig_output_file" {}
 
 variable "kubeconfig_template_file" {}
 
+variable "kubeconfig_output_file" {
+  default     = "kubeconfig.yaml"
+  description = "name of kubeconfig file to access cluster"
+}
+
+variable "kubeconfig_template_file" {
+  default     = "kubernetes/template/kubeconfig-template.yaml.tpl"
+  description = "template for generating kubernetes config file"
+}
