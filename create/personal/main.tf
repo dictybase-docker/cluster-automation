a1766551f1c23fe5026467c30af2d5cd8471b14d
wildlifehexagon <hartline.eric@gmail.com>
2021-05-13 13:35:47 -0500

    refactor: move kube-config to own folder

--- create/personal/main.tf
+++ create/personal/main.tf
@@ -16,12 +16,3 @@ module "gke-cluster" {
   image_type              = var.image_type
 }
 
-module "kubernetes-config" {
-  source                   = "./kubernetes"
-  cluster_name             = module.gke-cluster.cluster_name
-  cluster_id               = module.gke-cluster.cluster_id # creates dependency on cluster creation
-  cluster_endpoint         = module.gke-cluster.cluster_endpoint
-  cluster_ca_cert          = module.gke-cluster.cluster_ca_cert
-  kubeconfig_output_file   = var.kubeconfig_output_file
-  kubeconfig_template_file = var.kubeconfig_template_file
-}
