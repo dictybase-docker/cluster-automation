7f449c69f0ba7600c834ffcb6ce483e5332f662f
wildlifehexagon <hartline.eric@gmail.com>
2021-05-17 21:39:44 -0500

    refactor: move kubernetes module back into shared folder

---
+++ create/personal/kubernetes/vars.tf
@@ -0,0 +1,11 @@
+variable "cluster_name" {}
+
+variable "cluster_id" {}
+
+variable "cluster_endpoint" {}
+
+variable "cluster_ca_cert" {}
+
+variable "kubeconfig_output_file" {}
+
+variable "kubeconfig_template_file" {}
