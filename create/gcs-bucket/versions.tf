5bdc58431d2e5753ef3801d88fc7a42416d1f582
wildlifehexagon <hartline.eric@gmail.com>
2021-05-18 13:44:18 -0500

    chore: upgrade google provider version

--- create/gcs-bucket/versions.tf
+++ create/gcs-bucket/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "3.58.0"
+      version = "3.67.0"
     }
   }
   required_version = "~> 0.14"
