4c844bbd25241eb40a6adf9cc8d6e0279faeb09b
wildlifehexagon <hartline.eric@gmail.com>
2021-05-17 21:59:47 -0500

    fix: add kubeconfig vars

--- create/personal/kubernetes/vars.tf
+++ create/personal/kubernetes/vars.tf
@@ -6,6 +6,12 @@ variable "cluster_endpoint" {}
 
 variable "cluster_ca_cert" {}
 
-variable "kubeconfig_output_file" {}
+variable "kubeconfig_template_file" {
+  default     = "kubernetes/template/kubeconfig-template.yaml.tpl"
+  description = "kubeconfig template file"
+}
 
-variable "kubeconfig_template_file" {}
+variable "kubeconfig_output_file" {
+  default     = "kubeconfig.yaml"
+  description = "name of kubeconfig file to access cluster"
+}
--- create/personal/main.tf
+++ create/personal/main.tf
@@ -17,11 +17,9 @@ module "gke-cluster" {
 }
 
 module "kubernetes-config" {
-  source                   = "./kubernetes"
-  cluster_name             = module.gke-cluster.cluster_name
-  cluster_id               = module.gke-cluster.cluster_id # creates dependency on cluster creation
-  cluster_endpoint         = module.gke-cluster.cluster_endpoint
-  cluster_ca_cert          = module.gke-cluster.cluster_ca_cert
-  kubeconfig_output_file   = var.kubeconfig_output_file
-  kubeconfig_template_file = var.kubeconfig_template_file
+  source           = "./kubernetes"
+  cluster_name     = module.gke-cluster.cluster_name
+  cluster_id       = module.gke-cluster.cluster_id # creates dependency on cluster creation
+  cluster_endpoint = module.gke-cluster.cluster_endpoint
+  cluster_ca_cert  = module.gke-cluster.cluster_ca_cert
 }
